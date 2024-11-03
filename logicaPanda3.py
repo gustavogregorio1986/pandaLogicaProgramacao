c17b92506d4380b5e6dd298a8115fa40f08db52d
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-11-03 09:47:55 -0300

    desenvolvimento de sisrtema com python utilizanod panda paar  crescer e desenvolver

---
+++ logicaPanda3.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+dadxos = {'nome':['luiz','Ricardo','Pedro','Marcel','marcelo','mirele'],
+          'idade':[32,12,43,12,16,32],
+          'sexo':['m','m','f','m','f']}
+
+print(dadxos)
